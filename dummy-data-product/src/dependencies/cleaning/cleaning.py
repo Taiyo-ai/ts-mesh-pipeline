1c7139e449c0eb11480d6b1c6a6f2ef58417d71c
Mohammad Sharique <52289871+Sharique-stack@users.noreply.github.com>
2021-10-15 22:13:16 +0530

    Update cleaning.py

--- dummy-data-product/src/dependencies/cleaning/cleaning.py
+++ dummy-data-product/src/dependencies/cleaning/cleaning.py
@@ -1,26 +1,53 @@
 
-data_content = []
-for row in rows:
-    cells = row.find_all('td')
-    if len(cells) > 1:
-        print(cells[1].get_text())
-        country_link = cells[1].find('a')
-        country_info = [cell.text.strip('\n') for cell in cells]
-        additional_details = getAdditionalDetails(country_link.get('href'))
-        if (len(additional_details) == 4):
-            country_info += additional_details
-            data_content.append(country_info)
+import pandas as pd
 
-dataset = pd.DataFrame(data_content)
 
-# Define column headings
-headers = rows[0].find_all('th')
-headers = [header.get_text().strip('\n') for header in headers]
-headers += ['Total Area', 'Percentage Water', 'Total Nominal GDP', 'Per Capita GDP']
-dataset.columns = headers
+ class Cleaner:
+     def __init__(self):
+         return None
 
-drop_columns = ['Rank', 'Date', 'Source']
-dataset.drop(drop_columns, axis = 1, inplace = True)
-dataset.sample(3)
+     def load_data(self, file_loc):
+         data = pd.read_csv(file_loc)
+         return data
 
-dataset.to_csv("Dataset.csv", index = False)
+     def remove_cols(self, data, cols):
+         data.drop(cols, axis=1, inplace=True)
+         return data
+
+     def clean_location(self, data):
+         data["location"].replace(
+             {
+                 "Glenorcky": "Glenorchy",
+                 "Antwerp2": "Antwerp",
+                 "Multiple Locations": "Vancouver",
+             },
+             inplace=True,
+         )
+         return data
+
+     def clean_country(self, data):
+         data["country"] = data["country"].apply(lambda x: x.replace("\xa0", ""))
+         return data
+
+     def check_remove_duplicates(self, data):
+         data.drop_duplicates(keep="first", inplace=True)
+         data.reset_index(drop=True, inplace=True)
+         return data
+
+     def save_data(self, data, filename):
+         data.to_csv(filename, index=False)
+         return None
+
+
+ if __name__ == "__main__":
+     clean = Cleaner()
+     fred_step1 = clean.load_data("FRED_data.csv")
+     fred_step2 = clean.remove_cols(fred_step1, ["Null", "contributor"])
+     fred_step3 = clean.check_remove_duplicates(fred_step2)
+     fred_step4 = clean.clean_location(fred_step3)
+     fred_step5 = clean.clean_country(fred_step4)
+     clean.save_data(fred_step5, "clean_fred_data.csv")
+     header_step1 = clean.load_data("header_fred_data.csv")
+     header_step2 = clean.remove_cols(header_step1, ["extra"])
+     header_step3 = clean.check_remove_duplicates(header_step2)
+     clean.save_data(header_step3, "clean_header_fred_data.csv")
