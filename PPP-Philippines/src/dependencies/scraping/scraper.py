98cd71d6fc0f9dbd4d3e3e4b18d5dea1d711f6d4
RajeshNN <rrkk9944@gmail.com>
2021-10-29 14:47:21 +0530

    Edited scraper

--- PPP-Philippines/src/dependencies/scraping/scraper.py
+++ PPP-Philippines/src/dependencies/scraping/scraper.py
@@ -2,36 +2,56 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-def main():
-    root = 'https://ppp.gov.ph/project-database/'
+class MyClass:
+    def __init__(self, **kwargs):
+        self.config = kwargs.get("config")
 
-    basepage = requests.get(root)
-    soup = BeautifulSoup(basepage.text, 'html.parser')
+    def extract_links(self):
+        """Extract links with relevant data"""
+        root = 'https://ppp.gov.ph/project-database/'
 
-    links = []
-    for td in soup.find_all('td', class_ = 'title'):
-        for anchor in td.find_all('a'):
-            links.append(anchor['href'])
+        basepage = requests.get(root)
+        soup = BeautifulSoup(basepage.text, 'html.parser')
 
-    df = pd.DataFrame()
-    x = 0
-    for link in links:
-        x += 1
-        if x ==5:
-            break
-        info_pg = requests.get(link)
-        info_soup = BeautifulSoup(info_pg.text, 'html.parser')
-
-        temp = {}
-        temp['Name of Project'] = info_soup.find('h2', class_ = 'post-title').text.strip()
-        for i in info_soup.find_all('h6'):
-            for sib in i.next_siblings:
-                if sib.name == 'p':
-                    temp[str(i.text.strip())] = sib.text.strip()
-                elif sib.name == 'h6':
+        links = []
+        for td in soup.find_all('td', class_ = 'title'):
+            for anchor in td.find_all('a'):
+                links.append(anchor['href'])
+            if self.config['count']:
+                if len(links) == self.config['count']:
                     break
-        df = df.append(temp, ignore_index=True)
-    return df
+        return links
 
-if __name__=='__main__':
-    main()
+    def load_data(self, links):
+        """Function to load data"""
+        df = pd.DataFrame()
+        for link in links:
+            info_pg = requests.get(link)
+            info_soup = BeautifulSoup(info_pg.text, 'html.parser')
+
+            temp = {}
+            temp['Name of Project'] = info_soup.find('h2', class_ = 'post-title').text.strip()
+            for i in info_soup.find_all('h6'):
+                for sib in i.next_siblings:
+                    if sib.name == 'p':
+                        temp[str(i.text.strip())] = sib.text.strip()
+                    elif sib.name == 'h6':
+                        break
+            df = df.append(temp, ignore_index=True)
+        return df
+
+    def save_data(self, df):
+        """Function to save data"""
+        df.to_csv('scraped_data.csv')
+
+    def run(self):
+        """Load data, do_something and finally save the data"""
+        links = self.extract_links()
+        df = self.load_data(links)
+        self.save_data(df)
+
+
+if __name__ == "__main__":
+    config = {'count': None}
+    obj = MyClass(config = config)
+    obj.run()
