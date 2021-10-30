6f412ac49a71ce7c116451ef949b529765d85b67
RajeshNN <rrkk9944@gmail.com>
2021-10-30 18:48:52 +0530

    prepared cleaning.py

---
+++ PPP-Philippines/src/dependencies/cleaning/cleaning.py
@@ -0,0 +1,33 @@
+import pandas as pd
+
+
+class MyClass:
+    def __init__(self, **kwargs):
+        self.config = kwargs.get("config")
+        self.df = self.config['df']
+
+    def clean_empty_cols(self):
+        """removing columns with mostly empty entries"""
+        self.df.drop('Indicative Cost', axis=1, inplace=True)
+        # self.df.drop('Winning Bidder/s')
+
+    def remove_duplicates(self):
+        """deleting rows with duplicate data"""
+        self.df.drop_duplicates(keep=False, inplace=True)
+
+    def drop_na(self):
+        """dropping rows with nan values"""
+        self.df.dropna()
+
+    def run(self):
+        """putting everything together"""
+        self.clean_empty_cols()
+        self.remove_duplicates()
+        self.drop_na()
+        return self.df
+
+
+if __name__ == "__main__":
+    config = {'df': pd.DataFrame()}
+    obj = MyClass(config=config)
+    obj.run()
