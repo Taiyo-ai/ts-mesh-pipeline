75c01735eab77cb23dec192f3fd0ef0d6fde2e44
RajeshNN <rrkk9944@gmail.com>
2021-11-01 00:28:27 +0530

    Edited cleaning.py

--- PPP-Philippines/src/client.py
+++ PPP-Philippines/src/client.py
@@ -6,10 +6,9 @@ from dependencies.cleaning import cleaning
 scraper_config = {'count': 5}
 scraper_obj = scraper.MyClass(config=scraper_config)
 df = scraper_obj.run()
-for col in df.columns:
-    print(col)
 
 # cleaning the data
 cleaning_config = {'df': df}
 cleaning_obj = cleaning.MyClass(config=cleaning_config)
-cleaning_obj.run()
+df = cleaning_obj.run()
+print(df)
--- PPP-Philippines/src/dependencies/cleaning/cleaning.py
+++ PPP-Philippines/src/dependencies/cleaning/cleaning.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import re
 
 
 class MyClass:
@@ -6,24 +7,54 @@ class MyClass:
         self.config = kwargs.get("config")
         self.df = self.config['df']
 
-    def clean_empty_cols(self):
-        """removing columns with mostly empty entries"""
-        self.df.drop('Indicative Cost', axis=1, inplace=True)
-        self.df.drop('Winning Bidder/s', axis=1, inplace=True)
+    def strip_special(self):
+        for index, row in self.df.iterrows():
+            if re.sub('[^A-Za-z0-9]+', '', row['Project Description']) == '':
+                row['Project Description'] = ''
+            try:
+                temp = re.sub(r'[a-z,]', '', row['Project Cost'].lower())
+                print(temp)
+                self.df['Project Cost'][index] = temp
+            except AttributeError:
+                pass
+        self.df.rename({'Project Cost': 'Project Cost(in million Php)'},
+                       axis=1, inplace=True)
+
+    def remove_col(self, col_name):
+        """function to remove columns with mostly empty entries"""
+        self.df.drop(col_name, axis=1, inplace=True)
 
     def remove_duplicates(self):
-        """deleting rows with duplicate data"""
+        """function to delete rows with duplicate data"""
         self.df.drop_duplicates(keep=False, inplace=True)
 
-    def drop_na(self):
-        """dropping rows with nan values"""
-        self.df.dropna()
+    def drop_nan(self, col_name='all'):
+        """function to drop rows with nan values.
+        If col_name is given, rows is NaN values in the corresponding
+        column are removed."""
+        if col_name == 'all':
+            self.df.dropna(inplace=True)
+        else:
+            self.df.dropna(subset=[col_name], inplace=True)
+
+    def replace_nan(self, replace_str, col_name='all'):
+        """function to replace nan values with given replace_str.
+        If col_name is given, NaN values in the corresponding column are
+        replaced with replace_str.
+        By default all NaN values are replaced."""
+        nan_value = float("NaN")
+        self.df.replace(to_replace='', value=replace_str, inplace=True)
+        if col_name == 'all':
+            self.df.replace(to_replace=nan_value, value=replace_str,
+                            inplace=True)
+        else:
+            self.df[self.df[col_name].isnull()] = replace_str
 
     def run(self):
-        """putting everything together"""
-        self.clean_empty_cols()
+        """using cleaning functions as per use case"""
         self.remove_duplicates()
-        self.drop_na()
+        self.strip_special()
+        self.replace_nan('TBD')
         return self.df
 
 
