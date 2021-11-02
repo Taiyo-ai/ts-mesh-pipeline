abc95603af4490189b735bdc1a89f2e09eee41d5
RajeshNN <rrkk9944@gmail.com>
2021-11-02 22:00:17 +0530

    minor changes

--- PPP-Philippines/src/dependencies/cleaning/cleaning.py
+++ PPP-Philippines/src/dependencies/cleaning/cleaning.py
@@ -64,7 +64,7 @@ class DataCleaner:
         else:
             self.df.dropna(subset=[col_name], inplace=True)
 
-    def replace_nan(self):
+    def replace_nan(self, replace_str):
         """function to replace nan values with given replace_str.
         If col_name is given, NaN values in the corresponding column are
         replaced with replace_str.
@@ -73,15 +73,15 @@ class DataCleaner:
         for col in self.df.columns:
             if col not in ['project_cost(in_million_php)',
                            'indicative_cost(in_million_php)']:
-                self.df[col].replace('', 'TBD', inplace=True)
-                self.df[col].replace('None', 'TBD', inplace=True)
-                self.df[col].replace(nan_value, 'TBD', inplace=True)
+                self.df[col].replace('', replace_str, inplace=True)
+                self.df[col].replace('None', replace_str, inplace=True)
+                self.df[col].replace(nan_value, replace_str, inplace=True)
 
     def run(self):
         """using cleaning functions as per use case"""
         self.remove_duplicates()
         self.remove_unwanted_char()
-        self.replace_nan()
+        self.replace_nan('TBD')
         self.change_col_type()
         return self.df
 
