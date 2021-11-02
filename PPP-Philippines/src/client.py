1df871ef51b26138ab5e377a892a5fa39335a554
RajeshNN <rrkk9944@gmail.com>
2021-11-03 01:45:39 +0530

    Prepared geocoder.py

--- PPP-Philippines/src/client.py
+++ PPP-Philippines/src/client.py
@@ -1,16 +1,25 @@
 from dependencies.scraping import scraper
 from dependencies.cleaning import cleaning
+from dependencies.geocoding import geocoder
 
 
 # scraping the website
+print("Scraping the website...")
 scraper_config = {'count': 5}
 scraper_obj = scraper.PhilippinesProjects(config=scraper_config)
 df = scraper_obj.run()
 df.to_csv('../intermediate_files/scraped.csv')  # saving scraped data
 
 # cleaning the data
+print('Cleaning data...')
 cleaning_config = {'df': df}
 cleaning_obj = cleaning.DataCleaner(config=cleaning_config)
 df = cleaning_obj.run()
 df.to_csv('../intermediate_files/cleaned.csv')
-print(df['winning_bidder/s'])
+
+# geocoding regions
+print('Geocoding locations...')
+geocoding_config = {'df': df}
+geocoding_obj = geocoder.GeocodingRegion(config=geocoding_config)
+df = geocoding_obj.run()
+df.to_csv('../intermediate_files/geocoded.csv')
--- PPP-Philippines/src/dependencies/geocoding/geocoder.py
+++ PPP-Philippines/src/dependencies/geocoding/geocoder.py
@@ -1,8 +1,44 @@
 from geopy.geocoders import Nominatim
-geolocator = Nominatim(user_agent="rajesh-PPP_Philippines")
-location = geolocator.geocode("175 5th Avenue NYC")
-print(location.address)
-print()
-print((location.latitude, location.longitude))
-print()
-print(location.raw)
+import pandas as pd
+
+
+class GeocodingRegion:
+    def __init__(self, **kwargs):
+        self.config = kwargs.get("config")
+        self.df = self.config['df']
+        self.geolocator = Nominatim(user_agent="rajesh-PPP_Philippines")
+
+    def geo_code(self):
+        """geocoding region column if it is present"""
+        location_label = []
+        g_s_c = []
+        for index, row in self.df.iterrows():
+            try:
+                if row['region'] != 'TBD':
+                    pl = self.geolocator.geocode(str(row['region']) +
+                                                 ', Philippines',
+                                                 addressdetails=True)
+                    if not pl:
+                        address = row['region'].split('-')[0] + ', Philippines'
+                        pl = self.geolocator.geocode(address,
+                                                     addressdetails=True)
+                    location_label.append(list(pl.raw['address'].values())[0])
+                    g_s_c.append(f'{pl.latitude},{pl.longitude}')
+                else:
+                    location_label.append('TBD')
+                    g_s_c.append('TBD')
+            except KeyError:
+                pass
+        self.df['location_label'] = location_label
+        self.df['geo_spatial_coordinates'] = g_s_c
+
+    def run(self):
+        """running geo_code function"""
+        self.geo_code()
+        return self.df
+
+
+if __name__ == "__main__":
+    config = {'df': pd.DataFrame()}
+    obj = GeocodingRegion(config=config)
+    obj.run()
