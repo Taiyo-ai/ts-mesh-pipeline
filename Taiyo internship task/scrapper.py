d019e1285a0588633427241d60ffdcd615659677
Edgy Eft <48384216+aayushpandey1703@users.noreply.github.com>
2022-03-16 16:15:58 +0530

    Update scrapper.py

--- Taiyo internship task/scrapper.py
+++ Taiyo internship task/scrapper.py
@@ -1,14 +1,16 @@
 import csv
+from csv import writer
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
+import numpy as np
 
 class get_data:
 
   def __init__(self,soup):
     self.soup=soup
 
-  def get_dataset(self):
+  def get_dataset(self):                                                              # get list of datasets and links
     data=[]
     job=self.soup.find('div',id="cmsContent").find('ul').find_all('li')
     for i in job:
@@ -20,18 +22,28 @@ class get_data:
       w.writeheader()
       w.writerows(data)
 
-  def get_gdp_to_credit(self):
-    d={}
-    job=self.soup.find('div',id="cmsContent").find('ul').find('a',href='/statistics/full_credit_gap_csv.zip')
-    data=pd.read_csv('https://www.bis.org'+job['href'])
+  def get_gdp_to_credit(self):                                                       
+    d = {}
+    job = soup.find('div', id="cmsContent").find('ul').find('a', href='/statistics/full_credit_gap_csv.zip')
+    data = pd.read_csv('https://www.bis.org' + job['href'])
+    ref = [np.insert(data['Frequency'].unique(), 0, 'Frequency'),
+           np.insert(data["Borrowers' country"].unique(), 0, 'Country'),
+           np.insert(data['Borrowing sector'].unique(), 0, 'Borrowing Sector'),
+           np.insert(data['Lending sector'].unique(), 0, 'Lending Sector'),
+           np.insert(data['Credit gap data type'].unique(), 0, 'Credit gap data type')]
+    d['Time Period'] = data["Time Period"]
+
     for i in range(len(data)):
-      country=data["Borrowers' country"][i][3:]
+      country = data["Borrowers' country"][i][3:]
       if country not in d:
-        d[country]=[data["Time Period"][i]]
-      else:
-        d[country].append(data["Time Period"][i])
-    df=pd.DataFrame(d)
-    df.to_csv('csv_files/Credit_to_gdp_gaps.csv',index=False)
+        d[country] = ""
+    df = pd.DataFrame(d)
+
+    with open('csv_files/final_output.csv', 'w') as f:
+      w = writer(f)
+      w.writerows(ref)
+
+    df.to_csv('csv_files/final_output.csv', mode='a', index=False)
 
 html_text=requests.get('https://www.bis.org/statistics/full_data_sets.htm')
 soup=BeautifulSoup(html_text.text,'lxml')
