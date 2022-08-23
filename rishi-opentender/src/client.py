19c5ef2ad7cded5b865ab07478de6adfabf928c7
x7rishi <41459925+x7rishi@users.noreply.github.com>
2022-08-23 23:29:01 +0530

    updated printing of countrylist

--- rishi-opentender/src/client.py
+++ rishi-opentender/src/client.py
@@ -3,7 +3,7 @@ from dependencies.cleaning import cleaning
 from dependencies.utils import unzipper
 from dependencies.standardization import standardizer
 import os
-
+from pprint import pprint
 """ my scrapper class can work with list of country you specify to download otherwise give the alldownload = true
  and it will download the all tender zip file which is 3.9gb to proceed... 
 so that's why I used 'Malta' and 'cyprus' as example(dry run) (which have the lowest size among all country 
@@ -18,12 +18,12 @@ except FileExistsError:
 path = os.path.join(os.getcwd() + os.sep + 'data')
 
 """allDownload = true will download the 3.9gb all tender file but if it is false then use list of country to test the working project"""
-obj = scraper.Scrapper('https://opentender.eu/all/download', allDownload=False, country=['Denmark', 'Malta'])
+obj = scraper.Scrapper('https://opentender.eu/all/download', allDownload=False, country=['Cyprus', 'Malta'])
 resp = obj.MakeConn()
 obj.Soup(resp)
 clist = obj.makecountrylist('div', 'download-column download-headline', 'download-column download-csv',
                             'download-button')
-print(clist)  # generating the all countries tenderies in the opentender website
+pprint(clist)  # generating the all countries tenderies in the opentender website
 f = obj.download(path)
 un = unzipper.unzip(f, path)
 un.extract()  # this will extract all the files from the downloaded as in /data/{filename_country}
